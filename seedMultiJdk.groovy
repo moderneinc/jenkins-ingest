1d98c73e86ce4702c366272d00f7179faa83c45b
Greg Adams <greg@moderne.io>
2022-06-16 14:10:47 -0700

    .

--- seedMultiJdk.groovy
+++ seedMultiJdk.groovy
@@ -108,7 +108,7 @@ new File(workspaceDir, 'repos-multi-jdk.csv').splitEachLine(',') { tokens ->
         }
 
         steps {
-            sh(
+            shell(
 """
 export ANDROID_HOME=/usr/lib/android-sdk
 export ANDROID_SDK_ROOT=/usr/lib/android-sdk
