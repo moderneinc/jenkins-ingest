c3c337cf8df11380fcdb68a531341357b7487968
traceyyoshima <tracey.yoshima@gmail.com>
2022-07-01 15:06:22 -0700

    Added mvn jvm memory configuration

--- maven/add-extension.sh
+++ maven/add-extension.sh
@@ -3,6 +3,8 @@
 MVN_DIR=".mvn"
 EXT_FILE="extensions.xml"
 MVN_EXT_XML="<extension><groupId>com.gradle</groupId><artifactId>gradle-enterprise-maven-extension</artifactId><version>1.14.2</version></extension>"
+JVM_CONFIG="jvm.config"
+MVN_MEM ="Xmx2048m"
 # Create the .mvn directory if it does not exist.
 if [ ! -d "$MVN_DIR" ]; then
     mkdir "$MVN_DIR"
@@ -20,3 +22,16 @@ else
     ADD=$(echo $MVN_EXT_XML | sed "s/\\//\\\\\\//g")
     sed -i "/<\\/extensions>/ s/.*/${ADD}\\n&/" "$MVN_DIR/$EXT_FILE"
 fi
+
+# Create the jvm.config file if it does not exist.$
+if [ ! -f "$MVN_DIR/$JVM_CONFIG" ]; then
+  touch "$MVN_DIR/$JVM_CONFIG"
+  echo "-$MVN_MEM" >> "$MVN_DIR/$JVM_CONFIG"
+else
+  RESULT="$(grep -E Xmx[0-9]+.* $MVN_DIR/$JVM_CONFIG)"
+  if [ -z "$RESULT" ]; then
+    echo " -$MVN_MEM" >> "$MVN_DIR/$JVM_CONFIG"
+  else$
+    sed -i "s/Xmx[0-9]*[MmGg]/$MVN_MEM/" "$MVN_DIR/$JVM_CONFIG"
+  fi$
+fi$
