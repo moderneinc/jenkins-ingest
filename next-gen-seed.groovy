72803c5c487c5df4fa5aca740ae8f0f72836c08a
Tim te Beek <tim@moderne.io>
2023-02-13 10:37:30 +0100

    Strip 1. prefix from Java paths

--- next-gen-seed.groovy
+++ next-gen-seed.groovy
@@ -9,7 +9,7 @@ node {
 
     withCredentials([usernamePassword(credentialsId: 'artifactory', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
 
-        sh 'docker run -v '+ pwd() +':/repository -e JAVA_VERSION=1.8 -e PUBLISH_URL=https://artifactory.moderne.ninja/artifactory/moderne-ingest -e PUBLISH_USER='+ USERNAME + ' -e PUBLISH_PWD=' + PASSWORD +' moderne/moderne-cli:latest'
+        sh 'docker run -v '+ pwd() +':/repository -e JAVA_VERSION=8 -e PUBLISH_URL=https://artifactory.moderne.ninja/artifactory/moderne-ingest -e PUBLISH_USER='+ USERNAME + ' -e PUBLISH_PWD=' + PASSWORD +' moderne/moderne-cli:latest'
     }
 
 }
