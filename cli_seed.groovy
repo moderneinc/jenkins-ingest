5aaec350dc0859d66d238ba0c7e351ca753a9c88
rpau <raquel@moderne.io>
2023-02-28 12:18:44 +0100

    removing wrappers because they are designed to be directly executed

--- cli_seed.groovy
+++ cli_seed.groovy
@@ -85,32 +85,6 @@ new File(workspaceDir, 'repos.csv').splitEachLine(',') { tokens ->
                 }
             }
         
-            conditionalSteps {
-                condition {
-                    (fileExists('build.gradle', BaseDir.WORKSPACE) || fileExists('build.gradle.kts', BaseDir.WORKSPACE)) && !(fileExists('gradlew', BaseDir.WORKSPACE) || fileExists('gradlew.bat', BaseDir.WORKSPACE))
-                }
-                steps {
-                    gradle {
-                        useWrapper(false)
-                        gradleName 'gradle 7.4.2'
-                    }
-                }
-            } 
-            
-            conditionalSteps {
-                condition {
-                fileExists('pom.xml', BaseDir.WORKSPACE) && !( fileExists('mvnw', BaseDir.WORKSPACE) || fileExists('mvnw.cmd', BaseDir.WORKSPACE))    
-                }
-                steps {
-                configure { node ->
-                        node / 'builders' << 'org.jfrog.hudson.maven3.Maven3Builder' {
-                            useWrapper(false)
-                            mavenName 'maven3.x'
-                        }
-                }
-                }
-            } 
-        
             wrappers {
                 credentialsBinding {
                     usernamePassword('MODERNE_PUBLISH_USER', 'MODERNE_PUBLISH_PWD', publishCreds)
