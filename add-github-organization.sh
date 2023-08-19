ed402580468ccad0e67ce0804f7716d5c1d6b8cd
Tim te Beek <tim@moderne.io>
2023-08-19 15:50:11 +0200

    Archive additional projects for top 20 orgs (#147)

--- add-github-organization.sh
+++ add-github-organization.sh
@@ -15,6 +15,9 @@ gh repo list $1 --language groovy --archived --source --limit 1000 --json nameWi
 gh repo list $1 --language kotlin --archived --source --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 gh repo list $1 --language python --archived --source --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 
+# Quick analysis of largest organizations
+cut --delimiter='/' --fields=1 repos.csv | sort | uniq -c | sort -h | tail -n 20
+
 cd parser/
 ./gradlew build && java -cp build/libs/parser-1.0-SNAPSHOT.jar io.moderne.jenkins.ingest.Merger ../repos.csv  ../new.csv
 rm ../new.csv
