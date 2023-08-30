bb9924af3f4b30861e16f9a957fc87d1243ff110
Tim te Beek <tim@moderne.io>
2023-08-30 18:34:04 +0200

    Drop java input from workflow & script

--- add-github-organization.sh
+++ add-github-organization.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -ex
 
-java='java'
-if [ -n "$2" ]; then
-  java=$2
-fi
-
-gh repo list $1 --language java --no-archived --source --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,${java},,,,{{\"\n\"}}{{end}}" > new.csv
+gh repo list $1 --language java --no-archived --source --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{\"\n\"}}{{end}}" > new.csv
 gh repo list $1 --language python --no-archived --source --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{"\n"}}{{end}}' >> new.csv
 
 # Archive repos
