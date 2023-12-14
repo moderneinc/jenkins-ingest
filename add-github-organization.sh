f6d0a824ab1e1090838e40b173f3c5986e462c0b
Tim te Beek <tim@moderne.io>
2023-12-14 12:11:34 +0100

    Refresh google projects

--- add-github-organization.sh
+++ add-github-organization.sh
@@ -10,9 +10,9 @@ gh repo list $1 --language groovy --archived --source --visibility public --limi
 gh repo list $1 --language kotlin --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 gh repo list $1 --language python --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 
+# Merge new repos into existing repos
+(cd parser/; ./gradlew build && java -cp build/libs/parser-1.0-SNAPSHOT.jar io.moderne.jenkins.ingest.Merger ../repos.csv  ../new.csv)
+rm new.csv
+
 # Quick analysis of largest organizations
 cut --delimiter='/' --fields=1 repos.csv | sort | uniq -c | sort -h | tail -n 20
-
-cd parser/
-./gradlew build && java -cp build/libs/parser-1.0-SNAPSHOT.jar io.moderne.jenkins.ingest.Merger ../repos.csv  ../new.csv
-rm ../new.csv
