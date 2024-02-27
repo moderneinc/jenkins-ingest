883bb6e1d18c0567b218149d18bbfd22eca04eb1
Tim te Beek <tim@moderne.io>
2024-02-27 20:39:32 +0100

    Ingest DotNetAnalyzers (#214)

--- add-github-organization.sh
+++ add-github-organization.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -ex
 
-gh repo list $1 --language java --no-archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{\"\n\"}}{{end}}" > new.csv
+gh repo list $1 --language csharp --no-archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{\"\n\"}}{{end}}" > new.csv
+gh repo list $1 --language java --no-archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{\"\n\"}}{{end}}" >> new.csv
+gh repo list $1 --language kotlin --no-archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template "{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{\"\n\"}}{{end}}" >> new.csv
 gh repo list $1 --language python --no-archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,,{{"\n"}}{{end}}' >> new.csv
 
 # Archive repos
+gh repo list $1 --language csharp --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 gh repo list $1 --language java --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 gh repo list $1 --language groovy --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
 gh repo list $1 --language kotlin --archived --source --visibility public --limit 1000 --json nameWithOwner,defaultBranchRef --template '{{range .}},{{.nameWithOwner}},{{.defaultBranchRef.name}},,,,,,TRUE,archived{{"\n"}}{{end}}' >> new.csv
